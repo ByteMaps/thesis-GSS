24b097b0b12cb82ed9a01c74d25748aab506faf1
Jelle de Bruijn <jellejacobdebruijn@gmail.com>
2025-05-28 12:14:20 +0200

    reworked arrays, testing adjacency matrix

--- ABM/src/agent_class.py
+++ ABM/src/agent_class.py
@@ -9,15 +9,15 @@ class Individual(mesa.Agent):
 		super().__init__(model)
 
 		self.unique_id = unique_id
+		self.adjacency_row = model.adj_matrix[self.unique_id]
+
 		self.opinion = round(self.random.uniform(-1,1),3)
 		self.dist_createlink = dist_createlink
 		self.dist_removelink = dist_removelink
 
-		self.linked_agents = np.empty(model.num_agents)											# Initate array with zero links
-
 	def	find_neighbours(self, agentset):
 		"""Find all candidates to form links with and connect to them"""
-
+		candidates = np.copy(self.linked_agents)
 		candidates = [neighbour.unique_id for neighbour in agentset if abs(neighbour.opinion - self.opinion) <= self.dist_createlink and neighbour.unique_id != self.unique_id]
 
 		return candidates
@@ -27,3 +27,7 @@ class Individual(mesa.Agent):
 		
 		for candidate in candidates:										# Add candidate to linked agents # TODO add prob_create and tries_create to it
 			self.linked_agents[candidate.unique_id] = candidate
+
+	def	print_adjacency(self):
+		self.adjacency_row[self.adjacency_row != self.unique_id] = 1										# TODO index row properly (i,j) for uniqueID
+		print(f"Agent {self.unique_id} has row {self.adjacency_row}\n")
--- ABM/src/model_class.py
+++ ABM/src/model_class.py
@@ -6,13 +6,14 @@ class OpinionDynamicsModel(mesa.Model):
 	def __init__(self, N, Agent, dist_createlink, dist_removelink):
 		super().__init__()
 		self.num_agents = N
+		self.adj_matrix = np.zeros((N,N), dtype=float)											# Keep track of links between agents, optimised type
 
 		# Create agents
-		for i in range(self.num_agents):
-			agent = Agent(self, i, dist_createlink=dist_createlink, dist_removelink=dist_removelink)
+		for id in range(self.num_agents):
+			agent = Agent(self, id, dist_createlink=dist_createlink, dist_removelink=dist_removelink)
 
 	def	run(self):
 		"""Run through all agents to test functionality"""
-		
-		agents_opinions = [agent.linked_agents for agent in self.agents]
-		print(agents_opinions)
+		# print(self.adj_matrix)
+		self.agents.shuffle_do("print_adjacency")
+		print(self.adj_matrix)
