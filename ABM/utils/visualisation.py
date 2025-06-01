18668082534249aaa4aec9d10c9a0a42883e6b03
Jelle de Bruijn <jellejacobdebruijn@gmail.com>
2025-06-01 22:24:48 +0200

    copied first stage visualisation code

--- ABM/utils/visualisation.py
+++ ABM/utils/visualisation.py
@@ -1,4 +1,8 @@
 import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# TODO integrate code with model runs
 
 def	form_adjacency_matrix(agent_links, N):
 	"""Form the adjacency matrix for the agents in the model using the linked neighbour arrays"""
@@ -13,3 +17,28 @@ def	form_adjacency_matrix(agent_links, N):
 				adjacency_matrix[i, j] = 0
 
 	return adjacency_matrix
+
+def	form_edges(N, G, adj_matrix):
+	edges=[]
+	# Create a list of edges from the adjacency matrix
+	for i in range(N):
+		for j in range(i):
+			if G[i,j]==1:
+				edges.append((i,j))
+
+	return edges
+
+def	form_plot(edges):											# TODO sort out this code, comments & subtasks
+	cmap = plt.cm.viridis # plt.cm.hot #
+	G2 = nx.Graph()
+	G2.add_nodes_from(np.arange(0,N))
+	G2.add_edges_from(edges)
+	pos = nx.spring_layout(G2,seed=10)
+
+	plt.figure()
+	edgesdraw = nx.draw_networkx_edges(G2, pos, alpha=0.4)
+	nodesdraw = nx.draw_networkx_nodes(G2, pos, node_color=opinions, cmap=cmap, node_size=50, vmin=vmin, vmax=vmax)
+	sm = plt.cm.ScalarMappable(cmap=cmap, norm=plt.Normalize(vmin = vmin, vmax=vmax))
+	sm._A = []
+	cbar = plt.colorbar(sm, ax=plt.gca())
+	cbar.ax.tick_params(labelsize=15)
