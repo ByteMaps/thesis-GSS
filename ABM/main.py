12303971dff69457285a04ed64338737d9aa434d
Jelle de Bruijn <jellejacobdebruijn@gmail.com>
2025-06-26 15:55:02 +0200

    rm statsresults, added abstract. Goodbye.

--- ABM/main.py
+++ ABM/main.py
@@ -9,9 +9,10 @@ import psutil, datetime, time
 #================================================ PARAMETERS ==================================================================
 #							'''For more parameter options check parameters.py'''
 
-saveto 				= "extra"
-max_runtime			= 100
+saveto 				= "removedist_runtime_checks"
+max_runtime			= 1000
 
+total_runs			= 10
 param_amt 			= 10																	# Amount of different parameters to run
 sample 				= 100
 # poisson_set 		= np.linspace(0, 1, param_amt + 1)
@@ -26,6 +27,7 @@ def	run_model(params, modelrun):
 	print(f"{model.modeltype} {modelrun} cat: {model.final_cat} ran with lambda: {params.poisson_avg} in {model.total_runs} runs")
 
 def	check_usage(filepath):
+	"""Log the CPU usage every run"""
 	if not hasattr(psutil, "sensors_battery"):
 		pass
 	
@@ -39,16 +41,17 @@ def	check_usage(filepath):
 if __name__=="__main__":
 	print("Setting parameters")
 	params = GenT(runtime=max_runtime, path=saveto)
+	params.dist_removelink = 2.0								# ! Consensus test
 	
 	i = read_counter()
 
 	print(f"Initiating loop at {i}")
 	try:
-		while i < runs:
-			poisson_pick = round(np.random.random(),3) #round(poisson_set[i // sample], 3)
+		while i < total_runs:
+			poisson_pick = 0 # round(np.random.rand(),3) #round(poisson_set[i // sample], 3) # ! Consensus test
 			params.poisson_avg = poisson_pick
 			run_model(params, i)
-			check_usage('usage.csv')
+			# check_usage('usage.csv')
 			i += 1
 	except KeyboardInterrupt:
 		write_counter(i)
--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-Welcome to the repository that holds my thesis bachelor model! The model simulates Opinion Dynamics through 
-Agent-Based Modelling (hence the name of the root directory) with the base model remixed from Weinans et al. (2024) "An exploration of Drivers of Opinion Dynamics".
+Welcome to the repository that holds my thesis bachelor model! 
 
-My thesis revolves about Generational Turnover, or the influence on dynamic agent removal and addition over time and it's effect on OD.
+## Abstract
+This thesis investigates how generational turnover influences societal opinion dynamics using
+an Agent-Based Model. By extending an existing opinion dynamics model with stochastic agent
+replacement, this study explores how turnover impacts the emergence of consensus, coexistence, or
+polarisation. The results reveal that turnover increases the probability of coexistence and hinders
+consensus formation, offering insights for both modelling and policy design in dynamic social
+systems. Some of the main findings of this research are the increased probability of coexistence
+states and absence of consensus states, the increased stable model runtime, and the rise and drop
+of the average Degree over the turnover interval. Given the relevance of the field in sustainability,
+research to dissect stochastic factors and consensus drivers is needed.
+
+My work is based on the framework as laid out by Els Weinans et al. (2024) *An Exploration of Drivers of Opinion Dynamics*.
